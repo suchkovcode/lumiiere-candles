d5562f1920cc7e4197894b80c6678b8d21039cc0
Nikita Suchkov <suchkov8520@gmail.com>
2022-09-16 17:26:37 +0300

    Del: all components folder

--- src/components/canvas/_canvas.js
+++
@@ -1,8 +0,0 @@
-export function toggleMenuDrobdown() {
-   const drobdownItem = document.querySelectorAll(".canvas-menu__drobdown");
-   drobdownItem.forEach((element) => {
-      element.addEventListener("click", (e) => {
-         e.target.classList.toggle("active");
-      });
-   });
-}
