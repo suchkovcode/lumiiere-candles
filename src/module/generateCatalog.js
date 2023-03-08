e056806519cb96a1efbd59e4fd7e87534015a0db
Nikita Suchkov <suchkov8520@gmail.com>
2023-03-08 09:45:24 +0300

    Update: generateCatalog module

--- src/module/generateCatalog.js
+++ src/module/generateCatalog.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import { data } from "Root/assets/data/products";
-import svg from "Assets/img/svg/sprite.svg";
+import { Card } from "./Card";
 
 export class GenerateCatalog {
    constructor(target, data) {
@@ -18,74 +18,7 @@ export class GenerateCatalog {
 
    #html(data) {
       const markup = data.map((item, index) => {
-         return /* html */ `
-         <article class="card" data-index="${index}" data-id="${item.id}">
-         <header class="card__header">
-            <div class="card__favorite" data-type="favorite" data-select="false" data-id="${item.id}">
-               <svg class="card__favorite-icon">
-                  <use xlink:href="${svg}#cardFavoriteIcon"></use>
-               </svg>
-            </div>
-            <img class="card__hero" src="${item.hero}" loading="lazy" alt="" width="140" height="180" />
-         </header>
-         <main class="card__body">
-            <div class="card__rating">
-               <div class="card__rating-icons">
-                  <svg class="card__rating-icon" data-index="0">
-                     <use xlink:href="${svg}#cardRatingIcon"></use>
-                  </svg>
-                  <svg class="card__rating-icon" data-index="1">
-                     <use xlink:href="${svg}#cardRatingIcon"></use>
-                  </svg>
-                  <svg class="card__rating-icon" data-index="2">
-                     <use xlink:href="${svg}#cardRatingIcon"></use>
-                  </svg>
-                  <svg class="card__rating-icon" data-index="3">
-                     <use xlink:href="${svg}#cardRatingIcon"></use>
-                  </svg>
-                  <svg class="card__rating-icon" data-index="4">
-                     <use xlink:href="${svg}#cardRatingIcon"></use>
-                  </svg>
-               </div>
-               <span class="card__rating-value">${item.grade.level}</span>
-               <span class="card__rating-total">(${item.grade.count})</span>
-
-            </div>
-            <h3 class="card__title">${item.title}</h3>
-            <p class="card__article">
-               Артикул:
-               <span class="card__article-size" data-type="article">${item.code.small}</span>
-            </p>
-            <p class="card__description">${item.category.join(" | ").trim()}</p>
-            <p class="card__price" data-value="${item.price.small}">
-               <span class="card__price-currency">$</span>
-               <span class="card__price-value">${item.price.small}</span>
-            </p>
-            <div class="card__size">
-               <p class="card__size-title">Размер</p>
-               <div class="card__size-select">
-                  <button class="card__size-btn" type="button" data-id="${item.id}" data-index="0" data-value="40" data-price="${item.price.small}" data-select="true" >40 мл</button>
-                  <button class="card__size-btn" type="button" data-id="${item.id}" data-index="1" data-value="100" data-price="${item.price.medium}" data-select="false" >100 мл</button>
-                  <button class="card__size-btn" type="button"  data-id="${item.id}" data-index="2" data-value="200" data-price="${item.price.large}" data-select="false">200 мл</button>
-               </div>
-            </div>
-         </main>
-         <footer class="card__footer">
-            <a class="card__btn-more" href="#">Подробнее</a>
-            <div class="card__btn-count" data-type="count" data-value="1">
-               <button class="card__btn-minus" data-type="minus"></button>
-               <p class="card__count-value">1</p>
-               <button class="card__btn-plus" data-type="plus"></button>
-            </div>
-            <button class="card__btn-add" data-type="add">
-               <svg class="card__btn-add-icon">
-                  <use xlink:href="${svg}#cardBtnAddIcon"></use>
-               </svg>
-               <span>В корзину</span>
-            </button>
-         </footer>
-      </article>
-         `;
+         return new Card(item, index).html();
       });
       return markup.join("");
    }
@@ -93,8 +26,7 @@ export class GenerateCatalog {
    #render(arrData) {
       try {
          this._target.innerHTML = this.#html(arrData);
-      } catch (err) {
-      }
+      } catch (err) {}
    }
 
    #selectFavorite() {
@@ -207,4 +139,4 @@ export class GenerateCatalog {
 
 setTimeout(() => {
    new GenerateCatalog("#popular-catalog", data);
-}, 1500);
+}, 2500);
