3b7bf3ff0dcaeb9888b2aead6605d540ec892ce5
Nikita Suchkov <suchkov8520@gmail.com>
2023-05-09 18:00:37 +0300

    Update: slider components

--- src/module/class/slider.js
+++ src/module/class/slider.js
@@ -2,22 +2,66 @@ export class Slider {
    constructor(selector) {
       this.el = document.querySelector(selector);
       this.item = this.el.querySelectorAll(".slider-item");
+      this.dots = this.el.querySelector(".slider-dots");
       this.init();
    }
 
    init() {
-      const sliderItem = this.item;
+      this.#dots();
+      const dotsItem = this.el.querySelectorAll(".slider-dots__item");
       let index = 0;
       setInterval(() => {
          index++;
-         sliderItem.forEach((element) => element.classList.remove("active"));
+         this.item.forEach((element) => this.#sliderhHide(element));
+         dotsItem.forEach((element) => this.#dotHide(element));
 
-         if (index < sliderItem.length) {
-            sliderItem[index].classList.add("active");
+         if (index < this.item.length) {
+            this.#sliderShow(index);
+            this.#dotShow(index);
          } else {
             index = 0;
-            sliderItem[index].classList.add("active");
+            this.#sliderShow(index);
+            this.#dotShow(index);
          }
       }, 5000);
+   }
+
+   #dots() {
+      for (let index = 0; index < this.item.length; index++) {
+         const second = index === 0 ? "active" : "";
+         const html = `<span class="slider-dots__item ${second}" data-index="${index}"></span>`;
+         this.dots.insertAdjacentHTML("beforeend", html);
+      }
+      this.#dotsChange();
+   }
+
+   #dotsChange() {
+      const dotsItem = this.el.querySelectorAll(".slider-dots__item");
+      dotsItem.forEach((element) => {
+         element.addEventListener("click", (event) => {
+            const { index } = event.target.dataset;
+            this.item.forEach((element) => this.#sliderhHide(element));
+            dotsItem.forEach((element) => this.#dotHide(element));
+            this.#sliderShow(index);
+            this.#dotShow(index);
+         });
+      });
+   }
+
+   #dotShow(index) {
+      const dotsItem = this.el.querySelectorAll(".slider-dots__item");
+      dotsItem[index].classList.add("active");
+   }
+
+   #dotHide(item) {
+      item.classList.remove("active");
+   }
+
+   #sliderShow(indexItem) {
+      this.item[indexItem].classList.add("active");
+   }
+
+   #sliderhHide(item) {
+      item.classList.remove("active");
    }
 }
