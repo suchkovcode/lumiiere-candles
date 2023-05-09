f76fda57e2bc00bd0fd9a9a13d31d8127089887a
Nikita Suchkov <suchkov8520@gmail.com>
2023-05-09 18:09:09 +0300

    Update: scripts init modules

--- src/module/scripts.js
+++ src/module/scripts.js
@@ -1,17 +1,22 @@
-
 import "Root/routers/router";
 import "Root/module/class/route";
 import "Root/module/class/header";
 
 import { data } from "Root/assets/data/products";
 import { GenerateCatalog } from "Root/module/class/generateCatalog";
-import { Tabs } from "Root/module/class/tab";
 import { Slider } from "Root/module/class/slider";
+import { Tabs } from "Root/module/class/tab";
 
 window.addEventListener("DOMContentLoaded", () => {
    setTimeout(() => {
-      new GenerateCatalog("#popular-catalog", data);
-      new Slider(".slider-wrapper");
-      new Tabs(".tabs", ".tab-content");
+      try {
+         new GenerateCatalog("#popular-catalog", data);
+      } catch (err) {}
+      try {
+         new Slider(".slider-wrapper");
+      } catch (err) {}
+      try {
+         new Tabs(".tabs", ".tab-content");
+      } catch (err) {}
    }, 1500);
 });
