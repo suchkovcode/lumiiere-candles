21474db013bbca349088f9a22d50244420894db9
Nikita Suchkov <suchkov8520@gmail.com>
2023-06-07 09:57:37 +0300

    Update: imports script

--- src/module/scripts.js
+++ src/module/scripts.js
@@ -5,13 +5,13 @@ import { data } from "Root/assets/data/products";
 import { GenerateCatalog } from "Root/module/class/catalog/generateCatalog";
 import { Slider } from "Root/module/class/slider";
 import { Tabs } from "Root/module/class/tab";
-import { Backet } from "Root/module/class/backet";
-import { Favorite } from "Root/module/class/favorite";
+import { Backet } from "Root/module/class/canvas/backetCanvas";
+import { Favorite } from "Root/module/class/canvas/favoriteCanvas";
 
 window.addEventListener("DOMContentLoaded", () => {
    setTimeout(() => {
       new Backet().init();
-      new Favorite();
+      new Favorite().init();
       new GenerateCatalog("#popular-catalog", data).render();
       // new Slider(".slider-wrapper");
       // new Tabs(".tabs", ".tab-content");
