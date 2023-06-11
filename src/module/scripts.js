179deeedf0f9952416aeda6f5d257983bc31b81d
Nikita Suchkov <suchkov8520@gmail.com>
2023-06-11 11:25:38 +0300

    Add: header menu scripts

--- src/module/scripts.js
+++ src/module/scripts.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import "Root/routers/router";
 import "Root/module/class/route";
 
+import { HeaderMenu } from "Root/module/class/headerMenu";
 import { data } from "Root/assets/data/products";
 import { GenerateCatalog } from "Root/module/class/catalog/generateCatalog";
 import { Slider } from "Root/module/class/slider";
@@ -10,6 +12,7 @@ import { Favorite } from "Root/module/class/canvas/favoriteCanvas";
 
 window.addEventListener("DOMContentLoaded", () => {
    setTimeout(() => {
+      new HeaderMenu().init();
       new Backet().init();
       new Favorite().init();
       new GenerateCatalog("#popular-catalog", data).render();
