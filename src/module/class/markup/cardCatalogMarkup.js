78698bb278736b11f6fae19d2d9935e4167193bd
Nikita Suchkov <suchkov8520@gmail.com>
2023-06-12 11:56:59 +0300

    Add: reating components and update modules

--- src/module/class/markup/cardCatalogMarkup.js
+++ src/module/class/markup/cardCatalogMarkup.js
@@ -20,26 +20,26 @@ export class CardCatalog {
          <img class="card__hero" src="${this.item.hero}" loading="lazy" alt="Картинка продукта" width="140" height="180" />
       </header>
       <main class="card__body">
-         <div class="card__rating">
-            <div class="card__rating-icons">
-               <svg class="card__rating-icon" data-index="0">
+         <div class="rating">
+            <div class="rating__icons">
+               <svg class="rating__icon" data-index="0">
                   <use xlink:href="${svg}#cardRatingIcon"></use>
                </svg>
-               <svg class="card__rating-icon" data-index="1">
+               <svg class="rating__icon" data-index="1">
                   <use xlink:href="${svg}#cardRatingIcon"></use>
                </svg>
-               <svg class="card__rating-icon" data-index="2">
+               <svg class="rating__icon" data-index="2">
                   <use xlink:href="${svg}#cardRatingIcon"></use>
                </svg>
-               <svg class="card__rating-icon" data-index="3">
+               <svg class="rating__icon" data-index="3">
                   <use xlink:href="${svg}#cardRatingIcon"></use>
                </svg>
-               <svg class="card__rating-icon" data-index="4">
+               <svg class="rating__icon" data-index="4">
                   <use xlink:href="${svg}#cardRatingIcon"></use>
                </svg>
             </div>
-            <span class="card__rating-value">${this.item.grade.level}</span>
-            <span class="card__rating-total">(${this.item.grade.count})</span>
+            <span class="rating__value">${this.item.grade.level}</span>
+            <span class="rating__total">(${this.item.grade.count})</span>
          </div>
          <h3 class="card__title">${this.item.title}</h3>
          <p class="card__article">
