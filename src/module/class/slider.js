4298787cf50593fcc04f919ec0023113b93a5a77
Nikita Suchkov <suchkov8520@gmail.com>
2023-06-12 10:15:44 +0300

    Update: slider methods

--- src/module/class/slider.js
+++ src/module/class/slider.js
@@ -1,47 +1,70 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-destructuring */
+// @ts-nocheck
 export class Slider {
    constructor(selector) {
-      this.el = document.querySelector(selector);
-      this.item = this.el.querySelectorAll(".slider-item");
-      this.dots = this.el.querySelector(".slider-dots");
-      this.init();
+      this.el = selector;
+      this.item = ".slider-item";
+      this.dots = ".slider-dots";
    }
 
-   init() {
-      this.#dots();
-      const dotsItem = this.el.querySelectorAll(".slider-dots__item");
-      let index = 0;
-      setInterval(() => {
-         index++;
-         this.item.forEach((element) => this.#sliderhHide(element));
-         dotsItem.forEach((element) => this.#dotHide(element));
+   init(renderUrl) {
+      this.#isRenderPage(renderUrl);
+      window.addEventListener("hashchange", () => {
+         this.#isRenderPage(renderUrl);
+      });
+   }
 
-         if (index < this.item.length) {
-            this.#sliderShow(index);
-            this.#dotShow(index);
-         } else {
-            index = 0;
-            this.#sliderShow(index);
-            this.#dotShow(index);
-         }
-      }, 5000);
+   #changeSlider() {
+      setTimeout(() => {
+         this.#dots();
+
+         const parentElement = document.querySelector(this.el);
+         const sliderItem = parentElement.querySelectorAll(this.item);
+         const dotsItem = parentElement.querySelectorAll(".slider-dots__item");
+         let index = 0;
+         setInterval(() => {
+            index++;
+            sliderItem.forEach((element) => this.#sliderhHide(element));
+            dotsItem.forEach((element) => this.#dotHide(element));
+
+            if (index < sliderItem.length) {
+               this.#sliderShow(index);
+               this.#dotShow(index);
+            } else {
+               index = 0;
+               this.#sliderShow(index);
+               this.#dotShow(index);
+            }
+         }, 5000);
+      }, 500);
    }
 
    #dots() {
-      for (let index = 0; index < this.item.length; index++) {
+      const parentElement = document.querySelector(this.el);
+      const sliderItem = parentElement.querySelectorAll(this.item);
+      const dotsContainer = parentElement.querySelector(this.dots);
+
+      for (let index = 0; index < sliderItem.length; index++) {
          const second = index === 0 ? "active" : "";
          const html = `<span class="slider-dots__item ${second}" data-index="${index}"></span>`;
-         this.dots.insertAdjacentHTML("beforeend", html);
+         dotsContainer.insertAdjacentHTML("beforeend", html);
       }
       this.#dotsChange();
    }
 
    #dotsChange() {
-      const dotsItem = this.el.querySelectorAll(".slider-dots__item");
+      const parentElement = document.querySelector(this.el);
+      const sliderItem = parentElement.querySelectorAll(this.item);
+      const dotsItem = parentElement.querySelectorAll(".slider-dots__item");
+
       dotsItem.forEach((element) => {
          element.addEventListener("click", (event) => {
             const { index } = event.target.dataset;
-            this.item.forEach((element) => this.#sliderhHide(element));
+
+            sliderItem.forEach((element) => this.#sliderhHide(element));
             dotsItem.forEach((element) => this.#dotHide(element));
+
             this.#sliderShow(index);
             this.#dotShow(index);
          });
@@ -49,7 +72,8 @@ export class Slider {
    }
 
    #dotShow(index) {
-      const dotsItem = this.el.querySelectorAll(".slider-dots__item");
+      const parentElement = document.querySelector(this.el);
+      const dotsItem = parentElement.querySelectorAll(".slider-dots__item");
       dotsItem[index].classList.add("active");
    }
 
@@ -58,10 +82,23 @@ export class Slider {
    }
 
    #sliderShow(indexItem) {
-      this.item[indexItem].classList.add("active");
+      const parentElement = document.querySelector(this.el);
+      const sliderItem = parentElement.querySelectorAll(this.item);
+      sliderItem[indexItem].classList.add("active");
    }
 
    #sliderhHide(item) {
       item.classList.remove("active");
+   }
+
+   #getFragmentUrl() {
+      const matchLocation = window.location.href.match(/#([^#]*)(\?.*)?$/)[1];
+      let fragmentLocation = "";
+      matchLocation === "" ? (fragmentLocation = "/") : (fragmentLocation = matchLocation);
+      return fragmentLocation;
+   }
+
+   #isRenderPage(renderUrl) {
+      return this.#getFragmentUrl() === renderUrl ? this.#changeSlider() : false;
    }
 }
