9bc71f2dd798d6ad751a25ce88d33e563849cbf9
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-02 22:07:08 +0300

    Update: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,14 @@
-const autoprefixer = require("autoprefixer");
-const postcsssortmediaqueries = require("postcss-sort-media-queries");
-
+const cssnano = require("cssnano");
 module.exports = () => {
    return {
-      plugins: [autoprefixer, postcsssortmediaqueries],
+      plugins: [
+         require("autoprefixer")(),
+         require("postcss-combine-media-query"),
+         require("postcss-combine-duplicated-selectors"),
+         cssnano([
+            "default",
+            { discardComments: { removeAll: true }, discardEmpty: true, discardDuplicates: true, minifyFontValues: true },
+         ]),
+      ],
    };
 };
