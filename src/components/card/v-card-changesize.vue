d3cc687a86b068238840fa1432a97f4485b23a85
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-04 13:00:18 +0300

    Format: chagesize template

--- src/components/card/v-card-changesize.vue
+++ src/components/card/v-card-changesize.vue
@@ -3,9 +3,7 @@
       <p class="changesize__title">Размер</p>
       <div class="changesize__select">
          <button class="changesize__btn" value="small" :class="{ active: selectBtn === 'small' }" @click="selectBtnHandler">40 мл</button>
-         <button class="changesize__btn" value="medium" :class="{ active: selectBtn === 'medium' }" @click="selectBtnHandler">
-            100 мл
-         </button>
+         <button class="changesize__btn" value="medium" :class="{ active: selectBtn === 'medium' }" @click="selectBtnHandler">100 мл</button>
          <button class="changesize__btn" value="large" :class="{ active: selectBtn === 'large' }" @click="selectBtnHandler">200 мл</button>
       </div>
    </div>
