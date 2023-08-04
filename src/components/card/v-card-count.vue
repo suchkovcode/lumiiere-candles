95183799c5fa7a28c0e19e52fcf0fd7a732f32a4
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-04 15:21:07 +0300

    Update: countCard component

--- src/components/card/v-card-count.vue
+++ src/components/card/v-card-count.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="btncount">
-      <button class="btncount__minus" aria-label="Btn card minus" @click="countIncrement"></button>
+      <button class="btncount__minus" aria-label="Btn card minus" @click="countDecrement"></button>
       <p class="btncount__value">{{ countItem }}</p>
-      <button class="btncount__plus" aria-label="Btn card plus" @click="countDecrement"></button>
+      <button class="btncount__plus" aria-label="Btn card plus" @click="countIncrement"></button>
    </div>
 </template>
 
@@ -27,11 +27,11 @@ export default {
    },
 
    methods: {
-      countDecrement() {
+      countIncrement() {
          this.countItem === 10 ? (this.countItem = 10) : this.countItem++;
       },
 
-      countIncrement() {
+      countDecrement() {
          this.countItem === 1 ? (this.countItem = 1) : this.countItem--;
       },
    },
