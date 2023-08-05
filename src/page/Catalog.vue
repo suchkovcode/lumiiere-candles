b39636b395706743fcd9827b52c750ba766c17f1
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-05 20:55:52 +0300

    Del: pagination styles

--- src/page/Catalog.vue
+++ src/page/Catalog.vue
@@ -37,7 +37,7 @@
 </template>
 
 <script>
-import { mapActions, mapState } from "pinia";
+import { mapState } from "pinia";
 import { useAppStore } from "@/store/appStore";
 
 export default {
@@ -292,42 +292,6 @@ export default {
 
    @include lg {
       padding: 30px;
-   }
-}
-
-.pagination {
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   gap: 10px;
-
-   &__item {
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 40px;
-      height: 40px;
-      border: 1px solid var(--color_border);
-      border-radius: 6px;
-      font-family: "Karton";
-      font-size: 14px;
-      font-weight: 400;
-      color: var(--color_secondary);
-      transition: all 0.2s ease-in-out;
-
-      &:hover {
-         border: 1px solid var(--color_primary);
-         color: var(--color_base);
-         background-color: var(--color_primary);
-      }
-
-      &.active {
-         border: 1px solid var(--color_primary);
-         font-weight: 600;
-         color: var(--color_base);
-         background-color: var(--color_primary);
-      }
    }
 }
 </style>
