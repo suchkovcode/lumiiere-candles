0efaaa5c9794049ef5c8b869b5ec9f827207d7b1
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-05 17:48:54 +0300

    Update: footer nav

--- src/components/single/MyFooter.vue
+++ src/components/single/MyFooter.vue
@@ -1,17 +1,10 @@
 <template>
-   <footer
-      class="footer"
-      id="footer">
+   <footer class="footer" id="footer">
       <div class="container">
          <div class="footer__grid">
             <div class="footer__logo">
-               <router-link
-                  class="footer__logo-link"
-                  to="/"
-                  aria-label="Logo link">
-                  <svg
-                     class="footer__logo-icon"
-                     fill="none">
+               <router-link class="footer__logo-link" to="/" aria-label="Logo link">
+                  <svg class="footer__logo-icon" fill="none">
                      <use xlink:href="@/assets/img/svg/sprite.svg#logo"></use>
                   </svg>
                </router-link>
@@ -29,15 +22,23 @@
                   <h2 class="footer__catalog-title">Каталог</h2>
                </div>
                <ul class="footer__catalog-nav">
-                  <li
-                     v-for="item in nav.firstNav"
-                     class="footer__catalog-nav-item"
-                     :key="item.id">
-                     <router-link
-                        class="footer__catalog-nav-link transition"
-                        :to="item.to">
-                        {{ item.name }}
-                     </router-link>
+                  <li class="footer__catalog-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/catalog"> Мелтсы </router-link>
+                  </li>
+                  <li class="footer__catalog-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/catalog"> Спички </router-link>
+                  </li>
+                  <li class="footer__catalog-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/catalog"> Наборы </router-link>
+                  </li>
+                  <li class="footer__catalog-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/catalog"> Открытки </router-link>
+                  </li>
+                  <li class="footer__catalog-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/catalog"> Бестселлеры </router-link>
+                  </li>
+                  <li class="footer__catalog-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/catalog"> Соевые свечи </router-link>
                   </li>
                </ul>
             </nav>
@@ -49,15 +50,26 @@
                   <h2 class="footer__client-title">ДЛЯ КЛИЕНТА</h2>
                </div>
                <ul class="footer__client-nav">
-                  <li
-                     v-for="item in nav.secondNav"
-                     class="footer__client-nav-item"
-                     :key="item.id">
-                     <router-link
-                        class="footer__catalog-nav-link transition"
-                        :to="item.to">
-                        {{ item.name }}
-                     </router-link>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/care"> Уход </router-link>
+                  </li>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/about"> О нас </router-link>
+                  </li>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/partners"> Сотрудничество </router-link>
+                  </li>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/faq"> Ответы на вопросы </router-link>
+                  </li>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/delivery"> Доставка и оплата </router-link>
+                  </li>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/oferta"> Договор оферты </router-link>
+                  </li>
+                  <li class="footer__client-nav-item">
+                     <router-link class="footer__catalog-nav-link transition" to="/policy"> Политика конфиденциальности </router-link>
                   </li>
                </ul>
             </nav>
@@ -68,17 +80,9 @@
                   </svg>
                   <h2 class="footer__contact-title upercase">Контакты</h2>
                </div>
-               <a
-                  class="footer__contact-tel transition"
-                  href="tel:9999999999">
-                  +9 (999) 999 99 99
-               </a>
+               <a class="footer__contact-tel transition" href="tel:9999999999"> +9 (999) 999 99 99 </a>
                <p class="footer__contact-tel-text">Поможем с выбором</p>
-               <a
-                  class="footer__contact-email transition"
-                  href="mailto:info@lumiiere.candles.com">
-                  info@lumiiere.candles.com
-               </a>
+               <a class="footer__contact-email transition" href="mailto:info@lumiiere.candles.com"> info@lumiiere.candles.com </a>
                <div class="footer__props">
                   <p class="footer__props-title upercase">Реквизиты:</p>
                   <p class="footer__props-info">
@@ -88,42 +92,27 @@
                   </p>
                </div>
                <div class="footer__social">
-                  <a
-                     class="footer__social-link"
-                     href="https://instagram.com/lumiiere.candles?igshid=YmMyMTA2M2Y="
-                     aria-label="Social link">
+                  <a class="footer__social-link" href="https://instagram.com/lumiiere.candles?igshid=YmMyMTA2M2Y=" aria-label="Social link">
                      <svg class="footer__social-icon">
                         <use xlink:href="@/assets/img/svg/sprite.svg#footerSocialLike"></use>
                      </svg>
                   </a>
-                  <a
-                     class="footer__social-link"
-                     href="https://vt.tiktok.com/ZSdGUYsFb/"
-                     aria-label="Social link">
+                  <a class="footer__social-link" href="https://vt.tiktok.com/ZSdGUYsFb/" aria-label="Social link">
                      <svg class="footer__social-icon">
                         <use xlink:href="@/assets/img/svg/sprite.svg#footerSocialTikTok"></use>
                      </svg>
                   </a>
-                  <a
-                     class="footer__social-link"
-                     href="https://t.me/lumiiere_candles"
-                     aria-label="Social link">
+                  <a class="footer__social-link" href="https://t.me/lumiiere_candles" aria-label="Social link">
                      <svg class="footer__social-icon">
                         <use xlink:href="@/assets/img/svg/sprite.svg#footerSocialTelegram"></use>
                      </svg>
                   </a>
-                  <a
-                     class="footer__social-link"
-                     href="https://wa.me/79037590430"
-                     aria-label="Social link">
+                  <a class="footer__social-link" href="https://wa.me/79037590430" aria-label="Social link">
                      <svg class="footer__social-icon">
                         <use xlink:href="@/assets/img/svg/sprite.svg#footerSocialWhatsApp"></use>
                      </svg>
                   </a>
-                  <a
-                     class="footer__social-link"
-                     href="https://taplink.cc/lumiierecandles"
-                     aria-label="Social link">
+                  <a class="footer__social-link" href="https://taplink.cc/lumiierecandles" aria-label="Social link">
                      <svg class="footer__social-icon">
                         <use xlink:href="@/assets/img/svg/sprite.svg#footerSocialTaplink"></use>
                      </svg>
@@ -141,11 +130,7 @@
                </svg>
             </div>
             <p class="footer__copy">© 2023 Lumiiere. Защищено авторским правом</p>
-            <a
-               class="footer__dev transition"
-               href="https://github.com/suchkovcode/">
-               Разработка сайта
-            </a>
+            <a class="footer__dev transition" href="https://github.com/suchkovcode/"> Разработка сайта </a>
          </div>
       </div>
    </footer>
