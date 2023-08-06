4b4eb151d84c5343bd387264719a68968b0402d8
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-06 10:28:17 +0300

    Update: cssCodeSplit properties

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
    build: {
       outDir: "../dist/",
       manifest: false,
-      cssCodeSplit: false,
+      cssCodeSplit: true,
    },
    resolve: {
       alias: {
