66c48d56c13c26339acebc1d56e92c48b989bddb
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-29 23:23:39 +0300

    Del: meta data

--- src/app.vue
+++ src/app.vue
@@ -5,11 +5,8 @@
 </template>
 
 <script>
-import ogImage from "@/assets/img/png/preview.png";
-
 export default {
    setup() {
-
       useHead({
          htmlAttrs: {
             lang: "ru",
@@ -18,20 +15,7 @@ export default {
 
       useSeoMeta({
          title: "Lumiiere Candles | App",
-
          viewport: "width=device-width, initial-scale=1",
-         meta: [
-            { hid: "description", name: "description", content: "Our natural and perfects products. We are here to show you that natural is perfect. This is what our products are." },
-            { hid: "og:title", property: "og:title", content: "Arsenijs Fabric | Home" },
-            { hid: "og:url", property: "og:url", content: "https://lumiiere-candles.com" },
-            { hid: "og:description", property: "og:description", content: "Our natural and perfects products. We are here to show you that natural is perfect. This is what our products are." },
-            { hid: "og:image", property: "og:image", content: ogImage },
-
-            { hid: "twitter:title", name: "twitter:title", content: "Arsenijs Fabric | Home" },
-            { hid: "twitter:url", name: "twitter:url", content: "https://lumiiere-candles.com" },
-            { hid: "twitter:description", name: "twitter:description", content: "Our natural and perfects products. We are here to show you that natural is perfect. This is what our products are." },
-            { hid: "twitter:image", name: "twitter:image", content: ogImage },
-         ],
          link: [{ hid: "canonical", rel: "canonical", href: "https://lumiiere-candles.com" }],
       });
 
