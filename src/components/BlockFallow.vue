dad0608d12470bddbda530fed97394ab538befbe
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-29 21:08:23 +0300

    Fix: block markup

--- src/components/BlockFallow.vue
+++ src/components/BlockFallow.vue
@@ -1,9 +1,9 @@
 <template>
    <div class="fallow">
-      <span class="fallow__header">
+      <div class="fallow__header">
          <img class="fallow__icon" src="@/assets/img/svg/home__follow-icon.svg" loading="lazy" alt="Иконка" width="100" height="43" />
          <h2 class="title fallow__title">Подпишитесь на нашу рассылку</h2>
-      </span>
+      </div>
       <p class="subtitle fallow__subtitle">
          Мы ценим ваше доверие и не хотим завалиться в спам. Просто иногда будем делиться с вами анонсом новиночек, классными акциями и персональными
          предложениями.
