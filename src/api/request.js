ac731f4e8c1dc9d526621b140b6aed586a606279
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-31 16:34:08 +0300

    add card api request

--- src/api/request.js
+++ src/api/request.js
@@ -4,3 +4,31 @@ export const Axios = axios.create({
    baseURL: "http://localhost:1337/api",
    headers: { "Content-Type": "application/json" },
 });
+
+export const getProduct = async (params) => {
+   const { data } = await Axios.get("/products", { params: params });
+
+   const arrayCard = data.data.map((item) => {
+      const {
+         Aroma: { data: aromaData },
+         Category: { data: categoryData },
+         Collection: { data: collectionData },
+         img,
+         tags,
+         ...attributes
+      } = item.attributes;
+
+      const tagNames = tags.map((tag) => tag.name);
+
+      return {
+         ...attributes,
+         img: img.data.attributes,
+         aroma: aromaData.attributes.name,
+         category: categoryData.attributes.name,
+         collection: collectionData.attributes.name,
+         tags: tagNames,
+      };
+   });
+
+   return { card: arrayCard, meta: data.meta };
+};
