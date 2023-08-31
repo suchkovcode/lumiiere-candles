8e2575ef7a6ad9364db5e07bfd1c6e14a3414ebe
Nikita Suchkov <suchkov8520@gmail.com>
2023-08-31 19:51:22 +0300

    Add: new filter props

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -10,14 +10,12 @@
             <span class="filter__name">{{ item.slice(0, 1).toUpperCase() + item.slice(1) }}</span>
          </label>
       </div>
-
       <div class="filter__title">
          <svg class="filter__title-icon">
             <use xlink:href="@/assets/img/svg/sprite.svg#catalogFilter"></use>
          </svg>
          <p class="filter__name">Фильтры</p>
       </div>
-
       <div class="filter__sort" :class="{ active: isClickSort }">
          <button class="filter__sort-btn" @click="isClickSort = !isClickSort">Сортировка</button>
          <div class="filter__sort-drobdown">
@@ -39,75 +37,21 @@
             </label>
          </div>
       </div>
-
       <div class="filter__sort" :class="{ active: isClickAroma }">
          <button class="filter__sort-btn" @click="isClickAroma = !isClickAroma">Аромат</button>
          <div class="filter__sort-drobdown filter__sort-drobdown--gap">
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Десертные" />
-               <span class="filter__name">Десертные</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Мужественные" />
-               <span class="filter__name">Мужественные</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Природные" />
-               <span class="filter__name">Природные</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Пряные" />
-               <span class="filter__name">Пряные</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Свежие" />
-               <span class="filter__name">Свежие</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Фруктовые" />
-               <span class="filter__name">Фруктовые</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Цветочные" />
-               <span class="filter__name">Цветочные</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" value="Ягодные" />
-               <span class="filter__name">Ягодные</span>
+            <label v-for="item in aromaData" :key="item" class="filter__label filter__label--checkbox">
+               <input v-model="filter.aroma" class="filter__checkbox" type="checkbox" :value="item" />
+               <span class="filter__name">{{ item }}</span>
             </label>
          </div>
       </div>
-
       <div class="filter__sort" :class="{ active: isClickCollection }">
          <button class="filter__sort-btn" @click="isClickCollection = !isClickCollection">Коллекция</button>
          <div class="filter__sort-drobdown filter__sort-drobdown--gap">
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Аниме" />
-               <span class="filter__name">Аниме</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Без персонажей" />
-               <span class="filter__name">Без персонажей</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Гарри Поттер" />
-               <span class="filter__name">Гарри Поттер</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Игры" />
-               <span class="filter__name">Игры</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Новый год" />
-               <span class="filter__name">Новый год</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Сериалы" />
-               <span class="filter__name">Сериалы</span>
-            </label>
-            <label class="filter__label filter__label--checkbox">
-               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" value="Фильмы" />
-               <span class="filter__name">Фильмы</span>
+            <label v-for="item in collectionData" :key="item" class="filter__label filter__label--checkbox">
+               <input v-model="filter.collection" class="filter__checkbox" type="checkbox" :value="item" />
+               <span class="filter__name">{{ item }}</span>
             </label>
          </div>
       </div>
@@ -121,6 +65,16 @@ import { useAppStore } from "@/store/appStore";
 export default {
    props: {
       categoryData: {
+         type: Array,
+         required: true,
+      },
+
+      aromaData: {
+         type: Array,
+         required: true,
+      },
+
+      collectionData: {
          type: Array,
          required: true,
       },
@@ -150,7 +104,7 @@ export default {
       },
    },
 
-   mounted() {
+   created() {
       this.updateStateFilter(this.filter);
    },
 
