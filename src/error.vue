024214bcff076b672f7958e81527f6fbd9487e7d
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-01 22:53:27 +0300

    Update seo tags

--- src/error.vue
+++ src/error.vue
@@ -25,8 +25,13 @@ export default {
          htmlAttrs: {
             lang: "ru",
          },
-         link: [
+         meta: [
             { "http-equiv": "X-UA-Compatible", "content": "IE=edge" },
+            { name: "apple-mobile-web-app-title", content: "Lumiiere Candles" },
+            { name: "apple-mobile-web-app-capable", content: "yes" },
+            { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+         ],
+         link: [
             { rel: "shortcut icon", type: "image/png", href: "/_nuxt/static/favicon.ico" },
             { rel: "icon", type: "image/png", sizes: "16x16", href: "/_nuxt/static/favicon-16x16.png" },
             { rel: "icon", type: "image/png", sizes: "32x32", href: "/_nuxt/static/favicon-32x32.png" },
@@ -41,16 +46,14 @@ export default {
             { rel: "apple-touch-icon", type: "image/png", sizes: "167x167", href: "/_nuxt/static/apple-touch-icon-167x167.png" },
             { rel: "apple-touch-icon", type: "image/png", sizes: "180x180", href: "/_nuxt/static/apple-touch-icon-180x180.png" },
             { rel: "apple-touch-icon", type: "image/png", sizes: "1024x1024", href: "/_nuxt/static/apple-touch-icon-1024x1024.png" },
-            { name: "apple-mobile-web-app-title", content: "Lumiiere Candles" },
-            { name: "apple-mobile-web-app-capable", content: "yes" },
-            { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+
             { hid: "canonical", rel: "canonical", href: "https://lumiiere-candles.com" },
          ],
-      }),
-         useSeoMeta({
-            title: "Lumiiere Candles | 404",
-            description: "Описание не длиннее 155 символов",
-         });
+      });
+      useSeoMeta({
+         title: "Lumiiere Candles | 404",
+         description: "Описание не длиннее 155 символов",
+      });
 
       defineRobotMeta();
    },
