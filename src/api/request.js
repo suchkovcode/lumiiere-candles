35a66c360ea400ffd71aff0d31cb7453fc56a31d
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-01 22:34:25 +0300

    Update api request url

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-   baseURL: "http://localhost:1337/api",
+   baseURL: "https://strapi.lumiiere-candles.com/api",
    headers: { "Content-Type": "application/json" },
 });
 
