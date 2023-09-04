dbc957885d2f738d93f91995dccee2b784dc78a0
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-04 20:09:53 +0300

    Update requst

--- src/api/request.js
+++ src/api/request.js
@@ -1,34 +1,130 @@
 import axios from "axios";
+import { useAppStore } from "@/store/appStore";
 
 export const Axios = axios.create({
-   baseURL: "https://strapi.lumiiere-candles.com/api",
+   baseURL: "http://127.0.0.1:1337/api",
    headers: { "Content-Type": "application/json" },
 });
 
 export const getProduct = async (params) => {
-   const { data } = await Axios.get("/products", { params: params });
+   try {
+      const response = await Axios.get("/products", { params });
+      const { data } = response;
 
-   const arrayCard = data.data.map((item) => {
-      const {
-         Aroma: { data: aromaData },
-         Category: { data: categoryData },
-         Collection: { data: collectionData },
-         img,
-         tags,
-         ...attributes
-      } = item.attributes;
+      const arrayCard = data.data.map((item) => {
+         const {
+            Aroma: { data: aromaData },
+            Category: { data: categoryData },
+            Collection: { data: collectionData },
+            img,
+            tags,
+            ...attributes
+         } = item.attributes;
+
+         const tagNames = tags.map((tag) => tag?.name || null);
+
+         return {
+            ...attributes,
+            img: img?.data?.attributes || {},
+            aroma: aromaData?.attributes?.name || null,
+            category: categoryData?.attributes?.name || null,
+            collection: collectionData?.attributes?.name || null,
+            tags: tagNames,
+         };
+      });
+
+      return { card: arrayCard, meta: data.meta };
+   } catch (error) {
+      console.error("Ошибка при получении данных о продукте", error);
+      throw error;
+   }
+};
+
+export const getProductOne = async (id, params) => {
+   try {
+      const response = await Axios.get(`/products/${id}`, { params });
+      const { data } = response;
+
+      const { Aroma, Category, Collection, img, tags, ...attributes } = data.data.attributes;
 
       const tagNames = tags.map((tag) => tag.name);
 
       return {
-         ...attributes,
-         img: img.data.attributes,
-         aroma: aromaData.attributes.name,
-         category: categoryData.attributes.name,
-         collection: collectionData.attributes.name,
-         tags: tagNames,
+         card: {
+            ...attributes,
+            img: img?.data?.attributes || {},
+            aroma: Aroma?.attributes?.name || null,
+            category: Category?.attributes?.name || null,
+            collection: Collection?.attributes?.name || null,
+            tags: tagNames,
+         },
+         meta: data.meta,
       };
-   });
+   } catch (error) {
+      console.error("Ошибка при получении данных о продукте", error);
+      throw error;
+   }
+};
 
-   return { card: arrayCard, meta: data.meta };
+export const getUniqueCategories = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+
+   const uniqueCategories = [...new Set(card.map((item) => item.category))]
+      .filter((category) => category !== null)
+      .sort((a, b) => b.localeCompare(a));
+
+   return uniqueCategories;
+};
+
+export const getUniqueCollection = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+
+   const uniqueCollections = [...new Set(card.map((item) => item.collection))]
+      .filter((collection) => collection !== null)
+      .sort((a, b) => b.localeCompare(a));
+
+   return uniqueCollections;
+};
+
+export const getUniqueAroman = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+
+   const uniqueAromas = [...new Set(card.map((item) => item.aroma))]
+      .filter((aroma) => aroma !== null)
+      .sort((a, b) => b.localeCompare(a));
+
+   return uniqueAromas;
+};
+
+export const getCandlesCard = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+   return card.filter((item) => item.category === "Свечи");
+};
+
+export const getMeltsCard = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+   return card.filter((item) => item.category === "Мелтсы");
+};
+
+export const getBoxesCard = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+   return card.filter((item) => item.category === "Наборы свечей");
+};
+
+export const getPostcardCard = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+   return card.filter((item) => item.category === "Открытки");
+};
+
+export const getPostcardMore = async () => {
+   const store = useAppStore();
+   const { card } = await getProduct(store.params);
+   return card.filter((item) => item.category === "Дополнительно");
 };
