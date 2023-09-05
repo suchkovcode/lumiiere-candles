63d4187073e4d2e505e7bb179a57492f3d9fd314
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-05 13:26:23 +0300

    Rename card components

--- src/components/card/index.vue
+++ src/components/card/index.vue
@@ -1,7 +1,7 @@
 <template>
    <article class="card">
       <header class="card__header">
-         <card-v-favorite :card-id="cardData.uid" :is-favorite="card.isFavorite" @is-favorite="card.isFavorite = $event" />
+         <card-favorite :card-id="cardData.uid" :is-favorite="card.isFavorite" @is-favorite="card.isFavorite = $event" />
          <img
             class="card__hero"
             :src="cardData.img.url"
@@ -12,17 +12,17 @@
             loading="lazy" />
       </header>
       <div class="card__body">
-         <card-v-rating :rating-storage="card" />
+         <card-rating :rating-storage="card" />
          <h3 class="card__title">{{ cardData.title }}</h3>
          <p class="card__article">Артикул: {{ cardArticle }}</p>
-         <p v-if="isCandles" class="card__description">{{ categoryJoin }}</p>
-         <card-v-price :price-new="cardPriceNew" :price-old="cardPriceOld" />
-         <card-v-changesize v-if="isCandles" :size-item-data="card.size" @cardsize="updateSize" />
+         <p v-if="isCandles || isMelts" class="card__description">{{ categoryJoin }}</p>
+         <card-price :price-new="cardPriceNew" :price-old="cardPriceOld" />
+         <card-changesize v-if="isCandles" :size-item-data="card.size" @cardsize="card.size = $event" />
       </div>
       <footer class="card__footer">
-         <card-v-btn-more :card-id="cardData.uid" />
-         <card-v-count :count-data="card.count" @count-item="updateCount" />
-         <card-v-btn-add :is-empty="cardData.isStock" @click="addCardBacket" />
+         <card-btn-more :card-id="cardData.uid" />
+         <card-count :count-data="card.count" @count-item="card.count = $event" />
+         <card-btn-add :is-empty="cardData.isStock" @click="addCardBacket" />
       </footer>
    </article>
 </template>
@@ -56,32 +56,30 @@ export default {
       },
 
       cardArticle() {
-         return this.cardData?.article[this.card.size]?.toUpperCase();
+         return this.cardData?.article?.[this.card.size]?.toUpperCase();
       },
 
       cardPriceNew() {
-         return this.cardData?.price?.new[this.card.size];
+         const newPrice = this.cardData?.price?.new?.[this.card.size];
+         return newPrice ? newPrice : false;
       },
 
       cardPriceOld() {
-         return this.cardData?.price.old ? this.cardData.price.old[this.card.size] : false;
+         const oldPrice = this.cardData?.price?.old?.[this.card.size];
+         return oldPrice ? oldPrice : false;
       },
 
       isCandles() {
-         return this.cardData?.category?.toLowerCase()?.trim() !== "свечи" ? false : true;
-      }
+         return this.cardData?.category?.toLowerCase()?.trim() === "свечи";
+      },
+
+      isMelts() {
+         return this.cardData?.category?.toLowerCase()?.trim() === "мелтсы";
+      },
    },
 
    methods: {
       ...mapActions(useBacketStore, { addBacketCard: "addCardBacket" }),
-
-      updateSize(data) {
-         this.card.size = data;
-      },
-
-      updateCount(data) {
-         this.card.count = data;
-      },
 
       addCardBacket() {
          if (this.cardData.isStock) {
