7e0be44720af31d20a3f9034cd2aec3d27dcd3dd
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-10 21:48:54 +0300

    Format breadcrumbs

--- src/components/base/breadcrumbs.vue
+++ src/components/base/breadcrumbs.vue
@@ -1,7 +1,7 @@
 <template>
    <nav class="breadcrumbs">
       <ol class="breadcrumbs__list" itemscope itemtype="http://schema.org/BreadcrumbList">
-         <li class="breadcrumbs__item" itemscope itemprop="itemListElement"  itemtype="http://schema.org/ListItem">
+         <li class="breadcrumbs__item" itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
             <nuxt-link class="breadcrumbs__link" to="/" itemscope itemprop="item" itemtype="http://schema.org/Thing" itemid="/">
                <span itemprop="name">Главная</span>
             </nuxt-link>
@@ -14,7 +14,13 @@
             <meta itemprop="position" content="2" />
          </li>
          <li class="breadcrumbs__item active">
-            <nuxt-link class="breadcrumbs__link" :to="roterLink.to" itemscope itemprop="item" itemtype="http://schema.org/Thing" :itemid="roterLink.to">
+            <nuxt-link
+               class="breadcrumbs__link"
+               :to="roterLink.to"
+               itemscope
+               itemprop="item"
+               itemtype="http://schema.org/Thing"
+               :itemid="roterLink.to">
                <span itemprop="name"> {{ roterLink.name }}</span>
             </nuxt-link>
             <meta v-if="!roterLink.catalog" itemprop="position" content="2" />
