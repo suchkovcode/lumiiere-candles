e2cd37e7453e25ddbb4318aa195f7b7ec234eeb7
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-12 21:29:53 +0300

    Recoment candles catalog page

--- src/pages/catalog/candles.vue
+++ src/pages/catalog/candles.vue
@@ -17,65 +17,65 @@
    </section>
    <section class="candlesCatalog">
       <div class="container">
-         <!-- <AppCatalog v-if="card.length > 0" :data-item="card" :visible-item="countProductCatalog" class="catalogs__card--grey" />
+         <AppCatalog v-if="card.length > 0" :data-item="card" :visible-item="countProductCatalog" class="catalogs__card--grey" />
          <button
             v-if="card.length > 4 && countProductCatalog < 16"
             class="catalog__btn-loading catalog__btn-loading--margin"
             @click="countProductCatalog += 4">
             Загрузить еще
          </button>
-         <p v-else-if="!card.length" class="emptyData">Список пуст</p> -->
+         <p v-else-if="!card.length" class="emptyData">Список пуст</p>
       </div>
    </section>
 </template>
 
 <script>
-// import { useAppStore } from "@/store/appStore";
+import { useAppStore } from "@/store/appStore";
 
-// export default {
-//    async setup() {
-//       const store = useAppStore();
-//       const config = useRuntimeConfig();
+export default {
+   async setup() {
+      const store = useAppStore();
+      const config = useRuntimeConfig();
 
-//       const { data } = await useFetch(`${config.public.STRAPI}/api/products`, {
-//          method: "GET",
-//          params: {
-//             "pagination[pageSize]": 16,
-//             "locale": store.params.locale,
-//          },
-//       });
+      const { data } = await useFetch(`${config.public.STRAPI}/api/products`, {
+         method: "GET",
+         params: {
+            "pagination[pageSize]": 16,
+            "locale": store.params.locale,
+         },
+      });
 
-//       const arrayCard = data.value.data.map((item) => {
-//          const {
-//             Aroma: { data: aromaData },
-//             Category: { data: categoryData },
-//             Collection: { data: collectionData },
-//             img,
-//             tags,
-//             ...attributes
-//          } = item.attributes;
+      const arrayCard = data.value.data.map((item) => {
+         const {
+            Aroma: { data: aromaData },
+            Category: { data: categoryData },
+            Collection: { data: collectionData },
+            img,
+            tags,
+            ...attributes
+         } = item.attributes;
 
-//          const tagNames = tags.map((tag) => tag?.name || null);
+         const tagNames = tags.map((tag) => tag?.name || null);
 
-//          return {
-//             ...attributes,
-//             img: img?.data?.attributes || {},
-//             aroma: aromaData?.attributes?.name || {},
-//             category: categoryData?.attributes?.name || {},
-//             collection: collectionData?.attributes?.name || {},
-//             tags: tagNames,
-//          };
-//       });
+         return {
+            ...attributes,
+            img: img?.data?.attributes || {},
+            aroma: aromaData?.attributes?.name || {},
+            category: categoryData?.attributes?.name || {},
+            collection: collectionData?.attributes?.name || {},
+            tags: tagNames,
+         };
+      });
 
-//       return {
-//          card: arrayCard.filter((item) => item.category === "Свечи"),
-//       };
-//    },
+      return {
+         card: arrayCard.filter((item) => item.category === "Свечи"),
+      };
+   },
 
-//    data() {
-//       return {
-//          countProductCatalog: 8,
-//       };
-//    },
-// };
+   data() {
+      return {
+         countProductCatalog: 8,
+      };
+   },
+};
 </script>
