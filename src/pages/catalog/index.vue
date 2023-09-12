5de902c448483d1921e4179ab5b0725a4b33eca2
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-12 22:28:35 +0300

    Add handler api

--- src/pages/catalog/index.vue
+++ src/pages/catalog/index.vue
@@ -58,31 +58,11 @@ export default {
             "locale": store.params.locale,
          },
       });
-
-      const arrayCard = data.value.data.map((item) => {
-         const {
-            Aroma: { data: aromaData },
-            Category: { data: categoryData },
-            Collection: { data: collectionData },
-            img,
-            tags,
-            ...attributes
-         } = item.attributes;
-
-         const tagNames = tags.map((tag) => tag?.name || null);
-
-         return {
-            ...attributes,
-            img: img?.data?.attributes || {},
-            aroma: aromaData?.attributes?.name || {},
-            category: categoryData?.attributes?.name || {},
-            collection: collectionData?.attributes?.name || {},
-            tags: tagNames,
-         };
-      });
+      
+      const card = useHandllerApi(data);
 
       return {
-         card: arrayCard,
+         card,
       };
    },
 
