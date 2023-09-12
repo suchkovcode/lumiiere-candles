60246bde4b78983393f2ac5cd500dad857c47eac
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-12 22:32:54 +0300

    Del api request

--- src/store/favoriteStore.js
+++ src/store/favoriteStore.js
@@ -1,5 +1,4 @@
 import { defineStore } from "pinia";
-import { getProductOne } from "@/api/request";
 import { useAppStore } from "@/store/appStore";
 
 export const useFavoriteStore = defineStore("favoriteStore", {
@@ -12,11 +11,11 @@ export const useFavoriteStore = defineStore("favoriteStore", {
    actions: {
       async addCardFavorite(cardId) {
          const store = useAppStore();
-         const { card } = await getProductOne(cardId, store.params);
+         // const { card } = await getProductOne(cardId, store.params);
          const isExistsCard = this.cards.some((item) => item.uid === cardId);
 
          if (!isExistsCard) {
-            this.cards.push(card);
+            // this.cards.push(card);
          }
       },
 
