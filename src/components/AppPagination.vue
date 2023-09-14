c7db9760c7bf8c7d2ca83b4e488f6b00d4820dd4
Nikita Suchkov <suchkov8520@gmail.com>
2023-09-14 16:33:07 +0300

    Update pagination component

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -1,11 +1,6 @@
 <template>
    <div v-if="paginationData > 1" class="pagination">
-      <button
-         v-for="n in paginationData"
-         :key="n"
-         class="pagination__item"
-         :class="{ active: n === pageNumber }"
-         @click="$emit('pageNumber', n), (pageNumber = n)">
+      <button v-for="n in displayedPagination" :key="n" class="pagination__item" :class="{ active: n === pageNumber }" @click="changePage(n)">
          {{ n }}
       </button>
    </div>
@@ -31,6 +26,35 @@ export default {
       return {
          pageNumber: this.pageData,
       };
+   },
+
+   computed: {
+      displayedPagination() {
+         const visibleItems = 5;
+         const halfVisible = Math.floor(visibleItems / 2);
+         let start = Math.max(1, this.pageNumber - halfVisible);
+         let end = Math.min(this.paginationData, start + visibleItems - 1);
+         const diff = end - start + 1;
+
+         if (diff < visibleItems) {
+            if (start === 1) {
+               end = visibleItems;
+            } else {
+               start = this.paginationData - visibleItems + 1;
+               end = this.paginationData;
+            }
+         }
+
+         return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+      },
+   },
+
+   methods: {
+      changePage(newPage) {
+         window.scrollTo(0, 350);
+         this.pageNumber = newPage;
+         this.$emit("pageNumber", newPage);
+      },
    },
 };
 </script>
