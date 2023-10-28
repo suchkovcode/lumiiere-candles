752c5fa44c10a5d0330c3c791d40d64da32c8a0f
Nikita Suchkov <suchkov8520@gmail.com>
2023-10-28 19:00:09 +0300

    Add dashboard index page

---
+++ src/pages/dashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+      <h1 style="text-align: center; padding: 20px">Welcome to your dashboard</h1>
+      <button class="btn" style="max-width: 300px; margin: 0 auto" @click="logOut">Выйти</button>
+   </div>
+</template>
+
+<script>
+import { mapActions } from "pinia";
+import { useAppStore } from "@/store/appStore";
+
+export default {
+   setup() {
+      definePageMeta({
+         layout: "auth",
+         middleware: "auth",
+      });
+   },
+
+   methods: {
+      ...mapActions(useAppStore, { logoutAuth: "logOut" }),
+
+      logOut() {
+         const { logout } = useStrapiAuth();
+         logout();
+         this.logoutAuth();
+         this.$router.push("/auth/login");
+      },
+   },
+};
+</script>
