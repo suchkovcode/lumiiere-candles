63e820c97eb398a7f78c6cf2ac515bd28fa83c35
Nikita Suchkov <suchkov8520@gmail.com>
2023-10-29 20:22:19 +0200

    Add admin page

---
+++ src/layouts/admin.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="admin">
+      <header class="admin__header">
+         <div class="container">
+            <div class="admin__header-wrap">
+               <div></div>
+               <nuxt-link class="admin__header-logo" to="/" aria-label="Logo link">
+                  <svg class="admin__header-icon">
+                     <use xlink:href="/sprite.svg#logo"></use>
+                  </svg>
+               </nuxt-link>
+               <button class="admin__header-btn" @click="logOut">Выйти</button>
+            </div>
+         </div>
+      </header>
+      <main id="admin">
+         <slot />
+      </main>
+   </div>
+</template>
+
+<script>
+export default {
+   methods: {
+      logOut() {
+         const { logout } = useStrapiAuth();
+         process.client ? localStorage.removeItem("username") : false;
+         logout();
+         this.$router.push("/auth/login");
+      },
+   },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/admin/admin";
+</style>
--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,21 +1,18 @@
 <template>
-   <div>
-      <h1 style="text-align: center; padding: 20px">Welcome to your dashboard</h1>
-      <button class="btn" style="max-width: 300px; margin: 0 auto" @click="logOut">Выйти</button>
-   </div>
+   <section>
+   </section>
 </template>
 
 <script>
 export default {
    setup() {
       definePageMeta({
-         layout: "auth",
+         layout: "admin",
          middleware: "auth",
       });
    },
 
    methods: {
-
       logOut() {
          const { logout } = useStrapiAuth();
          process.client ? localStorage.removeItem("username") : false;
