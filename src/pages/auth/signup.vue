a97036667c992d1a193f3263ad2ec8a9addadab8
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 16:13:34 +0200

    Migration of script to Composition api

--- src/pages/auth/signup.vue
+++ src/pages/auth/signup.vue
@@ -10,9 +10,9 @@
          </header>
          <div class="auth__body">
             <h1 class="auth__title">РЕГИСТРАЦИЯ</h1>
-            <VForm v-slot="{ meta: formMeta }" class="auth__form" :validation-schema="schema" :initial-values="initialValues" @submit="createUser">
+            <Form v-slot="{ meta: formMeta }" class="auth__form" :validation-schema="schema" :initial-values="initialValues" @submit="createUser">
                <div class="auth__field">
-                  <VField v-slot="{ field, meta }" name="username">
+                  <Field v-slot="{ field, meta }" name="username">
                      <input
                         v-bind="field"
                         class="auth__input"
@@ -20,12 +20,12 @@
                         placeholder="Username"
                         autocomplete="name"
                         :class="{ invalid: !meta.valid && meta.touched, valid: meta.valid && meta.touched }" />
-                  </VField>
-                  <VErrorMessage class="auth__input-err" name="username" as="span" />
+                  </Field>
+                  <ErrorMessage class="auth__input-err" name="username" as="span" />
                </div>
 
                <div class="auth__field">
-                  <VField v-slot="{ field, meta }" name="email">
+                  <Field v-slot="{ field, meta }" name="email">
                      <input
                         v-bind="field"
                         class="auth__input"
@@ -33,12 +33,12 @@
                         placeholder="Email"
                         autocomplete="email"
                         :class="{ invalid: !meta.valid && meta.touched, valid: meta.valid && meta.touched }" />
-                  </VField>
-                  <VErrorMessage class="auth__input-err" name="email" as="span" />
+                  </Field>
+                  <ErrorMessage class="auth__input-err" name="email" as="span" />
                </div>
 
                <div class="auth__field auth__field-password" :class="{ active: isVisible }">
-                  <VField v-slot="{ field, meta }" name="password">
+                  <Field v-slot="{ field, meta }" name="password">
                      <input
                         v-bind="field"
                         class="auth__input"
@@ -49,8 +49,8 @@
                      <svg class="auth__input-icon" @click="isVisible = !isVisible">
                         <use xlink:href="/sprite.svg#eye"></use>
                      </svg>
-                  </VField>
-                  <VErrorMessage class="auth__input-err" name="password" as="span" />
+                  </Field>
+                  <ErrorMessage class="auth__input-err" name="password" as="span" />
                </div>
                <div class="auth__info">
                   <p class="auth__info-text">Используйте 6 или более символов с комбинацией букв, цифр и символов</p>
@@ -68,8 +68,8 @@
                   :class="{ novalid: !formMeta.valid || !token }">
                   ЗАРЕГИСТРИРОВАТЬСЯ
                </button>
-               <p v-if="isValidvisible" class="auth__input-err auth__input-err--form">Ошибка регистрации, повторите ще раз</p>
-            </VForm>
+               <p v-if="isValidVisible" class="auth__input-err auth__input-err--form">Ошибка регистрации, повторите ще раз</p>
+            </Form>
             <div class="auth__with">
                <hr />
                <p class="auth__with-text">или</p>
@@ -99,59 +99,45 @@
    </section>
 </template>
 
-<script>
+<script setup>
 import { Form, Field, ErrorMessage } from "vee-validate";
 import { object, string } from "yup";
 
-export default {
-   components: {
-      VForm: Form,
-      VField: Field,
-      VErrorMessage: ErrorMessage,
-   },
-   setup() {
-      definePageMeta({
-         layout: "auth",
-      });
-   },
+const token = ref(null);
+const isValidVisible = ref(false);
+const isVisible = ref(false);
+const initialValues = ref({ username: "", email: "", password: "" });
 
-   data() {
-      return {
-         token: null,
-         isValidvisible: false,
-         isVisible: false,
-         initialValues: { username: "", email: "", password: "" },
-         schema: object({
-            username: string().trim().required("Обязательное поле").min(4, "Минимальное количество символов 4"),
-            email: string()
-               .trim()
-               .required("Обязательное поле")
-               .email("Введите правильный email")
-               .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, { message: "Введите полный email" }),
-            password: string().trim().required("Обязательное поле").min(6, "Минимальное количество символов 6"),
-         }),
-      };
-   },
+const schema = object({
+   username: string().trim().required("Обязательное поле").min(4, "Минимальное количество символов 4"),
+   email: string()
+      .trim()
+      .required("Обязательное поле")
+      .email("Введите правильный email")
+      .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, { message: "Введите полный email" }),
+   password: string().trim().required("Обязательное поле").min(6, "Минимальное количество символов 6"),
+});
 
-   methods: {
-      async createUser(values, actions) {
-         const { register } = useStrapiAuth();
-         try {
-            if (this.token) {
-               const response = await register({ username: values.username, email: values.email, password: values.password });
-               process.client ? localStorage.setItem("username", response.user.value.username) : false;
-               this.token = null;
-               this.$router.push("/admin");
-            }
-         } catch (e) {
-            (this.isValidvisible = true), console.error(e);
-            setTimeout(() => {
-               this.isValidvisible = false;
-            }, 2500);
-         } finally {
-            actions.resetForm();
-         }
-      },
-   },
+const createUser = async (values, actions) => {
+   const { register } = useStrapiAuth();
+   try {
+      if (token.value) {
+         const response = await register({ username: values.username, email: values.email, password: values.password });
+         process.client ? localStorage.setItem("username", response.user.value.username) : false;
+         token.value = null;
+         $router.push("/admin");
+      }
+   } catch (e) {
+      (isValidvisible.value = true), console.error(e);
+      setTimeout(() => {
+         sValidvisible.value = false;
+      }, 2500);
+   } finally {
+      actions.resetForm();
+   }
 };
+
+definePageMeta({
+   layout: "auth",
+});
 </script>
