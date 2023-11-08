3da85efb46a30afb7e13eda9dc7b612a74fcab6e
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 21:16:21 +0200

    Migration of script to Composition api

--- src/components/canvas/backet-item.vue
+++ src/components/canvas/backet-item.vue
@@ -1,53 +1,61 @@
 <template>
    <div class="backetItem">
-      <img class="backetItem__img" :src="backetItem.img.url" :alt="backetItem.img.alternativeText" :title="backetItem.img.caption" width="70" height="70" />
+      <img
+         class="backetItem__img"
+         :src="backetItem.img.url"
+         :alt="backetItem.img.alternativeText"
+         :title="backetItem.img.caption"
+         width="70"
+         height="70" />
       <div class="backetItem__options">
-         <nuxt-link class="backetItem__name" :to="`/catalog/${backetItem.id}`">{{ backetItem.title }}</nuxt-link>
+         <nuxt-link class="backetItem__name" :to="`/catalog/${pathToCategory}/${backetItem.id}`">{{ backetItem.title }}</nuxt-link>
          <p class="backetItem__size">Размер: {{ sizeArticleData }} мл</p>
          <p class="backetItem__code">{{ backetItem.article }}</p>
       </div>
       <div class="backetItem__count">
          <div class="backetItem__plusminus">
-            <div class="backetItem__minus" @click="decrementCount(backetItem.article)"></div>
+            <div class="backetItem__minus" @click="store.decrementCountCard(backetItem.article)"></div>
             <div class="backetItem__total">
                {{ backetItem.count }}
             </div>
-            <div class="backetItem__plus" @click="incrementCount(backetItem.article)"></div>
+            <div class="backetItem__plus" @click="store.incrementCountCard(backetItem.article)"></div>
          </div>
          <div class="backetItem__sum">
             {{ backetItem.total }} {{ backetItem.currency }}
          </div>
       </div>
-      <div class="backetItem__remove" @click="delcard(backetItem.article)"></div>
+      <div class="backetItem__remove" @click="store.delCardBacket(backetItem.article)"></div>
    </div>
 </template>
 
-<script>
-import { mapActions } from "pinia";
-import { useBacketStore } from "@/store/backetStore";
-
-export default {
-   props: {
-      backetItem: {
-         type: Object,
-         required: true,
-      },
+<script setup>
+const props = defineProps({
+   backetItem: {
+      type: Object,
+      required: true,
    },
+});
 
-   computed: {
-      sizeArticleData() {
-         const sizeMapping = {
-            small: "40",
-            medium: "100",
-            large: "200",
-         };
+const store = useBacketStore();
 
-         return sizeMapping[this.backetItem.size] || "";
-      },
-   },
+const sizeArticleData = computed(() => {
+   const sizeMapping = {
+      small: "40",
+      medium: "100",
+      large: "200",
+   };
 
-   methods: {
-      ...mapActions(useBacketStore, { incrementCount: "incrementCountCard", decrementCount: "decrementCountCard", delcard: "delCardBacket" }),
-   },
-};
+   return sizeMapping[props.backetItem.size] || "";
+});
+
+const pathToCategory = computed(() => {
+   const categoryToPath = {
+      Свечи: "candles",
+      Наборы: "boxes",
+      Мелтсы: "melts",
+      Дополнительно: "matches",
+   };
+
+   return categoryToPath[props.backetItem.category];
+});
 </script>
