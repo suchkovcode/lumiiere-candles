754d26921b7a6c33ee11358800a25f402fc5ee20
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 21:20:41 +0200

    Migration of script to Composition api

--- src/components/card/btn-more.vue
+++ src/components/card/btn-more.vue
@@ -2,31 +2,27 @@
    <nuxt-link class="cardBtnMore" :to="`/catalog/${pathToCategory}/${cardId}`"> Подробнее </nuxt-link>
 </template>
 
-<script>
-export default {
-   props: {
-      cardId: {
-         type: String,
-         required: true,
-      },
-
-      cardType: {
-         type: String,
-         required: true,
-      },
+<script setup>
+const props = defineProps({
+   cardId: {
+      type: String,
+      required: true,
    },
 
-   computed: {
-      pathToCategory() {
-         const categoryToPath = {
-            Свечи: "candles",
-            Наборы: "boxes",
-            Мелтсы: "melts",
-            Дополнительно: "matches",
-         };
-
-         return categoryToPath[this.cardType];
-      },
+   cardType: {
+      type: String,
+      required: true,
    },
-};
+});
+
+const pathToCategory = computed(() => {
+   const categoryToPath = {
+      Свечи: "candles",
+      Наборы: "boxes",
+      Мелтсы: "melts",
+      Дополнительно: "matches",
+   };
+
+   return categoryToPath[props.cardType];
+});
 </script>
