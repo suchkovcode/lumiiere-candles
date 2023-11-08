faf82633228db0fdbbf5495a0f3c880ab4427be6
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 15:36:33 +0200

    Migrating a script to the Composition API

--- src/pages/admin/index.vue
+++ src/pages/admin/index.vue
@@ -1,24 +1,10 @@
 <template>
-   <section>
-   </section>
+   <section></section>
 </template>
 
-<script>
-export default {
-   setup() {
-      definePageMeta({
-         layout: "admin",
-         middleware: "auth",
-      });
-   },
-
-   methods: {
-      logOut() {
-         const { logout } = useStrapiAuth();
-         process.client ? localStorage.removeItem("username") : false;
-         logout();
-         this.$router.push("/auth/login");
-      },
-   },
-};
+<script setup>
+definePageMeta({
+   layout: "admin",
+   middleware: "auth",
+});
 </script>
