78486bbe321a35b245cfa44787db1b2dab095cc4
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 18:56:01 +0200

    Migration of script to Composition api

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -1,54 +1,30 @@
 <template>
    <div v-if="paginationData > 1" class="pagination">
-      <button v-for="n in displayedPagination" :key="n" class="pagination__item" :class="{ active: n === pageNumber }" @click="changePage(n)">
+      <button v-for="n in displayedPagination" :key="n" class="pagination__item" :class="{ active: n === store.pageNumber }" @click="changePage(n)">
          {{ n }}
       </button>
    </div>
 </template>
 
-<script>
-export default {
-   props: {
-      paginationData: {
-         type: Number,
-         required: true,
-      },
-
-      pageData: {
-         type: Number,
-         required: true,
-      },
+<script setup>
+const store = useAppStore();
+const emit = defineEmits(["pageNumber"]);
+const props = defineProps({
+   paginationData: {
+      type: Number,
+      required: true,
    },
+});
 
-   emits: ["pageNumber"],
+const displayedPagination = computed(() => {
+   const visibleItems = Math.min(props.paginationData, 5);
+   const start = Math.max(1, Math.min(store.pageNumber - Math.floor(visibleItems / 2), props.paginationData - visibleItems + 1));
+   const end = Math.min(start + visibleItems - 1, props.paginationData);
+   return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+});
 
-   data() {
-      return {
-         pageNumber: this.pageData,
-      };
-   },
-
-   computed: {
-      displayedPagination() {
-         const visibleItems = Math.min(this.paginationData, 5);
-         const start = Math.max(1, Math.min(this.pageNumber - Math.floor(visibleItems / 2), this.paginationData - visibleItems + 1));
-         const end = Math.min(start + visibleItems - 1, this.paginationData);
-         return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-      },
-   },
-
-   watch: {
-      pageData(value) {
-         this.pageNumber = value;
-      },
-   },
-
-   methods: {
-      changePage(newPage) {
-         window.scrollTo(0, 350);
-         this.pageNumber = newPage;
-         this.$emit("pageNumber", newPage);
-      },
-   },
+const changePage = (newPage) => {
+   window.scrollTo(0, 350);
+   store.setPageNumber(newPage);
 };
 </script>
