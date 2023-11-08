d26f157d70d778b042e11645ba3158f57d0263b7
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 22:03:35 +0200

    Fix backet__product-all bug

--- src/components/canvas/backet.vue
+++ src/components/canvas/backet.vue
@@ -8,10 +8,10 @@
             <div class="backet__product-list">
                <canvas-backet-item v-for="item in store.cards" :key="item.id" :backet-item="item" />
             </div>
-            <p class="backet__product-all">Итого: {{ getSumAddition }} {{ cards[0].currency }}</p>
+            <p class="backet__product-all">Итого: {{ store.getSumAddition }} {{ store.cards[0].currency }}</p>
             <button class="btn backet__product-btn" type="button" @click="checkout = true">Оформить</button>
          </div>
-         <p v-show="!store.cards.length && !checkout" class="D">Корзина пуста</p>
+         <p v-show="!store.cards.length && !checkout" class="backet__empty">Корзина пуста</p>
          <canvas-backet-form v-if="checkout" :sum-data="store.sumCard" :currency-data="store.cards[0].currency" />
       </ClientOnly>
    </div>
