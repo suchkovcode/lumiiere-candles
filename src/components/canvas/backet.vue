d8bc55d2bcc43e0ad649636b4d9e576869be33ce
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-08 21:52:02 +0200

    Migration of script to composition api

--- src/components/canvas/backet.vue
+++ src/components/canvas/backet.vue
@@ -1,50 +1,31 @@
 <template>
    <div class="backet" :class="{ active: activeState }">
-      <canvas-header @close-canvas="emitCloseCanvas">
+      <canvas-header @close-canvas="$emit('closeCanvas', false)">
          Корзина:
       </canvas-header>
       <ClientOnly>
-         <div v-if="cards.length && !checkout" class="backet__product">
+         <div v-if="store.cards.length && !checkout" class="backet__product">
             <div class="backet__product-list">
-               <canvas-backet-item v-for="item in cards" :key="item.id" :backet-item="item" />
+               <canvas-backet-item v-for="item in store.cards" :key="item.id" :backet-item="item" />
             </div>
             <p class="backet__product-all">Итого: {{ getSumAddition }} {{ cards[0].currency }}</p>
             <button class="btn backet__product-btn" type="button" @click="checkout = true">Оформить</button>
          </div>
-         <p v-show="!cards.length && !checkout" class="backet__empty">Корзина пуста</p>
-         <canvas-backet-form v-if="checkout" :sum-data="getSumAddition" :currency-data="cards[0].currency" />
+         <p v-show="!storecards.length && !checkout" class="backet__empty">Корзина пуста</p>
+         <canvas-backet-form v-if="checkout" :sum-data="store.sumCard" :currency-data="store.cards[0].currency" />
       </ClientOnly>
    </div>
 </template>
 
-<script>
-import { mapState } from "pinia";
-import { useBacketStore } from "@/store/backetStore";
-
-export default {
-   props: {
-      activeState: {
-         type: Boolean,
-         required: true,
-      },
+<script setup>
+const emit = defineEmits(["closeCanvas"]);
+const props = defineProps({
+   activeState: {
+      type: Boolean,
+      required: true,
    },
+});
 
-   emits: ["closeCanvas"],
-
-   data() {
-      return {
-         checkout: false,
-      };
-   },
-
-   computed: {
-      ...mapState(useBacketStore, ["cards", "getSumAddition"]),
-   },
-
-   methods: {
-      emitCloseCanvas() {
-         this.$emit("closeCanvas", false);
-      },
-   },
-};
+const store = useBacketStore();
+const checkout = ref(false);
 </script>
