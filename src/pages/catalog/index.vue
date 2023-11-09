febf4bba4848a6af5e7b5eb33d7c457763bb76d1
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-09 19:34:11 +0200

    Fix sprite sourse

--- src/pages/catalog/index.vue
+++ src/pages/catalog/index.vue
@@ -4,12 +4,12 @@
          <base-breadcrumbs class="goods__breadcrumbs" :roter-link="{ name: 'Каталог', to: '/catalog' }" />
          <div class="goods__header">
             <svg class="goods__header-icon" width="85" height="40">
-               <use xlink:href="/sprite.svg#catalogHeaderIcon"></use>
+               <use xlink:href="@/assets/img/svg/sprite.svg#catalogHeaderIcon"></use>
             </svg>
             <h1 class="title goods__title">КАТАЛОГ</h1>
             <p class="subtitle goods__subtitle">
                <svg class="goods__subtitle-icon">
-                  <use xlink:href="/sprite.svg#catalogGetting"></use>
+                  <use xlink:href="@/assets/img/svg/sprite.svg#catalogGetting"></use>
                </svg>
                «Поверь дружок, здесь ты найдешь все для сохранения своего душевного равновесия»
             </p>
@@ -18,7 +18,7 @@
             <div class="goods__search-container">
                <input class="goods__search-input" type="text" placeholder="Что нужно найти?" @input="searchQueryData = $event.target.value" />
                <svg class="goods__search-icon" fill="none" stroke="rgb(182, 182, 182)">
-                  <use xlink:href="/sprite.svg#search"></use>
+                  <use xlink:href="@/assets/img/svg/sprite.svg#search"></use>
                </svg>
             </div>
             <button class="goods__search-btn">Найти</button>
