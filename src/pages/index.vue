b0e2c49b42184f41d3965afe3265ecd11f022814
Nikita Suchkov <suchkov8520@gmail.com>
2023-11-26 17:35:22 +0200

    Del block fallow component and add markup to index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -209,7 +209,21 @@
    </section>
    <section class="subscribe">
       <div class="container">
-         <BlockFallow />
+         <div class="fallow">
+            <img class="fallow__bg" src="@/assets/img/webp/home__subscribe-bg.webp" alt="picture" loading="lazy" width="300" height="595" />
+            <div class="fallow__header">
+               <img class="fallow__icon" src="@/assets/img/svg/fallow__header-icon.svg" alt="Иконка" width="100" height="43" />
+               <h2 class="title fallow__title">Подпишитесь на нашу рассылку</h2>
+            </div>
+            <p class="subtitle fallow__subtitle">
+               Мы ценим ваше доверие и не хотим завалиться в спам. Просто иногда будем делиться с вами анонсом новиночек, классными акциями и
+               персональными предложениями.
+            </p>
+            <div class="fallow__form">
+               <input class="fallow__input" type="email" name="email" placeholder="Ваш E-mail" autocomplete="email" required />
+               <button class="fallow__btn" type="button">Подписаться</button>
+            </div>
+         </div>
       </div>
       <svg class="hero__wave-2" width="11700" height="90">
          <use xlink:href="@/assets/img/svg/sprite.svg#wave-2"></use>
